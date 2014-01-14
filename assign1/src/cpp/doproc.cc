7e78daed8d81f4fd96424df18e13d4dd2970b504
Chen  Vincent Mao Mao <chenvin2@ug159.eecg>
2014-01-13 21:42:05 -0500

    blah

--- assign1/src/cpp/doproc.cc
+++ assign1/src/cpp/doproc.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 extern "C" { 
 #include <simple.h>
+#include "../labeltab.h"
 }
 
 #include<vector>
@@ -9,58 +10,99 @@ extern "C" {
 // refer to the following link as a starting point if you are not familiar with them: 
 // http://www.sgi.com/tech/stl/Vector.html
 // http://www.sgi.com/tech/stl/hash_map.html 
+void printcfg(std::vector< std::vector<int> > *cfg)
+{
+    std::vector< std::vector<int> > cfgraph = *cfg;
+   for(int i =0;i<cfgraph.size();i++)
+   {
+   	  printf("block %d\n",i);
+   	  for(int j =0; j<cfgraph[i].size();j++)
+   	  {
+        printf("%d \n",cfgraph[i][j]);
+   	  }
+   }
+}
+
 
 simple_instr* do_procedure (simple_instr *inlist, char *proc_name)
 {
-
+	printf("%s\n",proc_name);
 	simple_instr *i = inlist;
 
+	// print out the entry block
+	std::vector<std::vector<int> > cfList;
+	int instructionIndex = 0;
+	int blockIndex = 1; 
+	// insert the first item onto the list
+	std::vector<int> startingBlock;
+	startingBlock.push_back(0);
+	cfList.push_back(startingBlock);
+
+	//create the first instruction block
+	std::vector<int> newBlock;
+	cfList.push_back(newBlock);
+	// initilize the label tab
+	//init_labeltab();
+	
+	bool nextStatementNewBlock = false;
 	while(i){
 		switch(i->opcode)
 		{
-			case LOAD_OP: {
-			
-				break;
-			}
-
-			case STR_OP: {
-			
-				break;
-			}
-
-			case MCPY_OP: {
-			
-				break;
-			}
-
+			case CVT_OP:
+			case CPY_OP:
+			case NEG_OP:
+			case NOT_OP:
+			case LOAD_OP: 
+			case STR_OP: 
+			case MCPY_OP: 
 			case LDC_OP: {
-			
-			break;
+				cfList[blockIndex].push_back(instructionIndex);
+				instructionIndex ++;
 			}
 
 			case JMP_OP: {
-			
+				cfList[blockIndex].push_back(instructionIndex);
+				instructionIndex ++;
+
+				std::vector<int> newBlock;
+				cfList.push_back(newBlock);
+				blockIndex++;
 			break;
 			}
 
-			case BTRUE_OP:
+			case BTRUE_OP:  // branch if true
 			case BFALSE_OP: {
-		
+				cfList[blockIndex].push_back(instructionIndex);
+				instructionIndex ++;
+
+				std::vector<int> newBlock;
+				cfList.push_back(newBlock);
+				blockIndex++;
 			break;
 			}
 
-			case CALL_OP: {
+			case CALL_OP: { // call a function
 		
 			break;
 			}
 
 			case MBR_OP: {
-			
+				
+
 			break;
 			}
 
 			case LABEL_OP: {
 			//fprintf(fd, "%s:\n", s->u.label.lab->name);
+			// start a new block here
+				cfList[blockIndex].push_back(instructionIndex);
+				instructionIndex ++;
+
+				std::vector<int> newBlock;
+				cfList.push_back(newBlock);
+				//enter_label(i->u.label,blockIndex);
+				blockIndex++;
+
 			break;
 			}
 
@@ -69,28 +111,23 @@ simple_instr* do_procedure (simple_instr *inlist, char *proc_name)
 			break;
 			}
 
-			case CVT_OP:
-			case CPY_OP:
-			case NEG_OP:
-			case NOT_OP: {
-			/* unary base instructions */
-			
-			break;
-			}
-
 			default: {
 			/* binary base instructions */
+				printf("entered the default case, shouldn't be here\n");
 			
 			}
 		}
+
 		i = i->next;
 	}
     // build flow control graph 
 
     // find immediate dominators    
-
+	printcfg(&cfList);
     return inlist;
 }
+
+
 
 
 /*
---
+++ assign1/src/cpp/prog_ver.cc
@@ -0,0 +1,5 @@
+/* Generated automatically by the SUIF makefiles. */
+
+char *prog_ver_string = "(unnumbered test version)";
+char *prog_who_string = "compiled Mon Jan 13 21:41:49 EST 2014 by chenvin2 on ug159.eecg";
+char *prog_suif_string = "1.1.2";
